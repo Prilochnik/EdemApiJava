fd9ccf4739a4e74cf0ed81123f95356e14945b67
apostolus <neketapostolov@gmail.com>
2021-09-10 13:07:13 +0300

    geofix

--- src/main/kotlin/com/example/edemapi/service/GeoService.kt
+++ src/main/kotlin/com/example/edemapi/service/GeoService.kt
@@ -18,7 +18,7 @@ class GeoService {
     }
 
     fun getGeoByIp(ip : String): Geo? {
-        return WebClient.create("https://ua.sxgeo.city")
+        return WebClient.create("https://ru3.sxgeo.city/82J9U")
             .get()
             .uri("json/$ip")
             .retrieve()
